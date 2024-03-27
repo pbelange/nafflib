ef4c07a5bf1141ec97ad62773ad3ceabcac612ed
Philippe Bélanger <pbelanger@Philippes-Laptop.local>
2024-03-27 15:35:10 +0100

    removed duplicate functions

--- nafflib/indexing.py
+++ nafflib/indexing.py
@@ -172,25 +172,4 @@ def linear_combinations(frequencies,Qvec=[],max_n=10,max_alias=5,return_errors=F
     else:
         return n_values
 
-# Invariant calculations
-def pdq_avg_integral(A,n,idx=0):
-    return np.sum([nk[idx]*(np.abs(Ak)**2)/2 for Ak,nk in zip(A,n)])
-    
-
-def invariant(A_list,n_list,idx=0):
-    assert len(A_list) == len(n_list)
-    return np.sum([pdq_avg_integral(A_plane,n_plane,idx) for A_plane,n_plane in zip(A_list,n_list)])
-
-def invariant_6D(Ax,Ay,Az,nx,ny,nz):
-    Ix = invariant([Ax,Ay,Az],[nx,ny,nz],idx=0)
-    Iy = invariant([Ax,Ay,Az],[nx,ny,nz],idx=1)
-    Iz = invariant([Ax,Ay,Az],[nx,ny,nz],idx=2)
-    return Ix,Iy,Iz
-
-
-
-
-
-
-
 
